350140bf61ed623ce0531480d71e4cfe51461a68
smartdan <danielesabetta@gmail.com>
2017-01-19 17:17:19 +0100

    First commit
    C# Code for splitting PDF files. A3 and A4 pages into 2 different files.

---
+++ A3PagesFromPDF/MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace A3PagesFromPDF
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInput = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnInput
+            // 
+            this.btnInput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInput.Location = new System.Drawing.Point(498, 27);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(140, 33);
+            this.btnInput.TabIndex = 0;
+            this.btnInput.Text = "Seleziona PDF";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCheck.Location = new System.Drawing.Point(498, 76);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(140, 33);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Controlla pagine";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(26, 37);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(145, 13);
+            this.lblFile.TabIndex = 2;
+            this.lblFile.Text = "Seleziona file da controllare...";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 141);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnInput);
+            this.Name = "MainForm";
+            this.Text = "A3 Pages in PDF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblFile;
+    }
+}
+
---
+++ A3PagesFromPDF/MainForm.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.IO;
+
+namespace A3PagesFromPDF
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //PROPERTIES
+        public string FileName { get; set; }
+       
+        //HANDLERS
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            var fileuscita = ReadPdfFile(FileName);
+            MessageBox.Show("Elaborazione completa in " + fileuscita);
+        }
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            var bfd = new OpenFileDialog();
+            if (bfd.ShowDialog() == DialogResult.OK)
+            {
+                FileName = bfd.FileName;
+                lblFile.Text = FileName;
+            }
+        }
+
+        //METHODS
+        private string ReadPdfFile(string fileName)
+        {
+            var output = "";
+            StringBuilder text1 = new StringBuilder();
+            text1.AppendLine("PAGINE A3: ");
+            StringBuilder text2 = new StringBuilder();
+            text2.AppendLine("PAGINE A4: ");
+
+            if (File.Exists(fileName))
+            {
+                PdfReader pdfReader = new PdfReader(fileName);
+
+                for (int page = 1; page <= pdfReader.NumberOfPages; page++)
+                {
+                    var mediabox = pdfReader.GetPageSize(page);
+                    var cropbox = pdfReader.GetCropBox(page);
+
+                    /*
+                     * W 595,22 H 842 => A4
+                       W 842 H 1191   => A3
+                     */
+
+                    if (mediabox.Width == 842 && mediabox.Height == 1191)
+                        text1.Append(String.Format("{0},", page));
+                    else
+                        text2.Append(String.Format("{0},", page));
+
+                    Console.WriteLine(String.Format("PAGE SIZE FOR PAGE {0}: W {1} H {2}", page, mediabox.Width, mediabox.Height));
+                }
+                pdfReader.Close();
+                text1.Append(Environment.NewLine);
+                text2.Append(Environment.NewLine);
+
+                output = String.Format("{0}.txt", fileName);
+
+                try
+                {
+                    var a3file = String.Format("{0}_A3.pdf", fileName);
+                    SelectPages(FileName, text1.ToString().Replace("PAGINE A3: ", ""), a3file);
+
+                    var a4file = String.Format("{0}_A4.pdf", fileName);
+                    SelectPages(FileName, text2.ToString().Replace("PAGINE A4: ", ""), a4file);
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc.ToString());
+                }
+                File.WriteAllText(output, text1.ToString() + text2.ToString());
+
+            }
+            return output;
+        }
+        private void SelectPages(string inputPdf, string pageSelection, string outputPdf)
+        {
+            using (PdfReader reader = new PdfReader(inputPdf))
+            {
+                reader.SelectPages(pageSelection);
+
+                using (PdfStamper stamper = new PdfStamper(reader, File.Create(outputPdf)))
+                {
+                    stamper.Close();
+                }
+            }
+        }
+    }
+}
